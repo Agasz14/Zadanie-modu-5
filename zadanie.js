984b6eba9da0cb1753d3b15ff86f15fdaf2072c0
Agasz14 <agnieszka.szajewska@gmail.com>
2024-04-20 14:31:56 +0200

    modul 5

---
+++ zadanie.js
@@ -0,0 +1,29 @@
+// doubled numbers
+
+const doubleNumber = (numbers) => {
+numbers.map ((Number) = Number *2)
+return DoubleArray
+}
+
+const Number = [1,2,3,4,5]
+doubleNumber(Number);
+
+
+console.log (Number)
+console.log (doubleArray)
+
+
+// students
+
+const students = [
+{name: "Student 1", surname: "Surname 1"},
+{name: "Student 2", surname: "Surname 2"},
+{name: "Student 3", surname: "Surname 3"},
+{name: "Student 4", surname: "Surname 4"},
+],
+
+const name = students.map (student => student.name);
+
+console.log (students)
+console.log (name)
+
