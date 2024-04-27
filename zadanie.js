ca54ed482d9b448cd7864b2a165c374732ae68eb
Agasz14 <agnieszka.szajewska@gmail.com>
2024-04-27 15:03:21 +0200

    corrected

--- zadanie.js
+++ zadanie.js
@@ -1,28 +1,26 @@
 // doubled numbers
 
 const doubleNumber = (numbers) => {
-numbers.map ((Number) => Number *2)
-return DoubledArray
-}
+    const DoubledArray = numbers.map((number) => number * 2);
+    return DoubledArray;
+};
+    const number = [1,2,3,4,5]
+    
+    console.log (number)
+    console.log (doubleNumber(number))
+    
+    
+    // students
+    
+    const students = [
+        {name: "Student 1", surname: "Surname 1"},
+        {name: "Student 2", surname: "Surname 2"},
+        {name: "Student 3", surname: "Surname 3"},
+        {name: "Student 4", surname: "Surname 4"},
+        ];
 
-const numbers = [1,2,3,4,5]
-doubleNumber(numbers);
+    const nameStudents =
+        students.map((student) => student.name);
 
-console.log (Numbers)
-console.log (doubledArray)
-
-
-// students
-
-const students = [
-{name: "Student 1", surname: "Surname 1"},
-{name: "Student 2", surname: "Surname 2"},
-{name: "Student 3", surname: "Surname 3"},
-{name: "Student 4", surname: "Surname 4"},
-];
-
-const name = students.map (student => student.name);
-
-console.log (students)
-console.log (name)
-
+    console.log (students)
+    console.log (nameStudents)
